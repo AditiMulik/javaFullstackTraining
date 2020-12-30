e5101557b7ac98b28fa98b09ff10d23dca9c23a1
Aditi M <aditijag12@gmail.com>
2020-12-30 23:32:37 +0530

    Removing exception dto

--- Milestone4/src/main/java/com/wf/training/bootapp/exception/GlobalHandler.java
+++ Milestone4/src/main/java/com/wf/training/bootapp/exception/GlobalHandler.java
@@ -1,24 +1,13 @@
 package com.wf.training.bootapp.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.wf.training.bootapp.dto.exception.ExceptionResponse;
 
 @RestControllerAdvice
 public class GlobalHandler {
 	
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ExceptionResponse> handler(Exception ex){
-		ExceptionResponse exception = 
-				new ExceptionResponse(ex.getMessage(),
-									  System.currentTimeMillis(),
-									  HttpStatus.INTERNAL_SERVER_ERROR.value());
-		ResponseEntity<ExceptionResponse> response =
-				new ResponseEntity<ExceptionResponse>(exception, HttpStatus.INTERNAL_SERVER_ERROR);
+	public String handler(Exception ex){
+		String response=null;
 		return response;
 	}
 }
